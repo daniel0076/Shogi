120f7b5d2bf574136d7ac6868f1a3e81c4e14407
Daniel Tsai <idaniel.twc@gmail.com>
2019-11-28 23:59:00 +0800

    feat(routing): add app routing

--- ShogiUI/src/app/app-routing.module.ts
+++ ShogiUI/src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './board/board.component';
+import { AuthComponent } from './core/auth/auth.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
+const routes: Routes = [
+  {
+    path: 'index',
+    component: WelcomeComponent
+  },
+  {
+    path: 'board',
+    component: BoardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- ShogiUI/src/app/app.module.ts
+++ ShogiUI/src/app/app.module.ts
@@ -5,17 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgxsModule } from '@ngxs/store';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BoardComponent } from './board/board.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    WelcomeComponent
   ],
   imports: [
+    CoreModule,
+    SharedModule,
     BrowserModule,
     AppRoutingModule,
-    NgxsModule.forRoot([])
+    NgxsModule.forRoot([]),
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
